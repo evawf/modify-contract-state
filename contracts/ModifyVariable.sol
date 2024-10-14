b3abf7f26da2ab84f866971906506f32cd08c1b4
eva.wang <eva.wang81@gmail.com>
2024-10-14 10:27:44 +0800

    test done

--- contracts/ModifyVariable.sol
+++ contracts/ModifyVariable.sol
@@ -3,13 +3,16 @@ pragma solidity ^0.8.4;
 
 contract ModifyVariable {
   uint public x;
+  string public y;
 
-  constructor(uint _x) {
+  constructor(uint _x, string memory _y) {
     x = _x;
+    y = _y;
   }
 
   function modifyToLeet() public {
     x = 1337;
+    y = "hello";
   }
 
 }
--- test/sample-test.js
+++ test/sample-test.js
@@ -9,7 +9,7 @@ describe("TestModifyVariable", function () {
     const ModifyVariable = await ethers.getContractFactory("ModifyVariable");
 
     // we then use the ContractFactory object to deploy an instance of the contract
-    const contract = await ModifyVariable.deploy(10);
+    const contract = await ModifyVariable.deploy(10, "bonjour");
 
     // wait for contract to be deployed and validated!
     await contract.waitForDeployment();
@@ -18,6 +18,9 @@ describe("TestModifyVariable", function () {
     await contract.modifyToLeet();
     // getter for state variable x
     const newX = await contract.x();
+    const newS = await contract.y();
+
     assert.equal(Number(newX), 1337);
+    assert.equal(String(newS), "hello");
   });
 });
